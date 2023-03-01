e03897e0049e5500fbf604d2c38701b384263e83
Stas Terehin <da.da@bk.ru>
2023-03-01 18:00:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 160000;
+        int y = 20;
+        int result = amount / y;
+        System.out.println(result);
+    }
+}
